c606df1ed5caca761b35213eb454246413e3af4a
ZP4 <jasonhuzheming9@gmail.com>
2019-12-12 17:27:33 -0500

    add admin field when creating user doc

--- src/store/database/asynchHandler.js
+++ src/store/database/asynchHandler.js
@@ -22,11 +22,12 @@ export const registerHandler = (newUser, firebase) => (dispatch, getState, { get
     const firestore = getFirestore();
     firebase.auth().createUserWithEmailAndPassword(
         newUser.email,
-        newUser.password,
+        newUser.password
     ).then(resp => firestore.collection('users').doc(resp.user.uid).set({
         firstName: newUser.firstName,
         lastName: newUser.lastName,
         initials: `${newUser.firstName[0]}${newUser.lastName[0]}`,
+        admin: 0
     })).then(() => {
         dispatch(actionCreators.registerSuccess);
     }).catch((err) => {
