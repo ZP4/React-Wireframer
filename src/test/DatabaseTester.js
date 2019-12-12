6b9cbd9d609b0fa4b4fdc5dcbc5b300278b5ef91
ZP4 <jasonhuzheming9@gmail.com>
2019-12-12 17:34:53 -0500

    fixed potential memory leak with a boolean check on componenent mount

--- src/test/DatabaseTester.js
+++ src/test/DatabaseTester.js
@@ -2,45 +2,83 @@ import React from 'react'
 import { connect } from 'react-redux';
 import todoJson from './TestTodoListData.json'
 import { getFirestore } from 'redux-firestore';
+import {Redirect} from "react-router-dom";
 
 class DatabaseTester extends React.Component {
+    _isMounted= true;
+    state = {
+        seconds: 5,
+
+    };
+
+    componentDidMount() {
+        if(this._isMounted){
+            this.myInterval = setInterval(() => {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            }, 1000);
+        }
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
 
     // NOTE, BY KEEPING THE DATABASE PUBLIC YOU CAN
     // DO THIS ANY TIME YOU LIKE WITHOUT HAVING
     // TO LOG IN
     handleClear = () => {
-        const fireStore = getFirestore();
-        fireStore.collection('todoLists').get().then(function(querySnapshot){
-            querySnapshot.forEach(function(doc) {
-                console.log("deleting " + doc.id);
-                fireStore.collection('todoLists').doc(doc.id).delete();
-            })
-        });
-    }
+        // const fireStore = getFirestore();
+        // fireStore.collection('todoLists').get().then(function(querySnapshot){
+        //     querySnapshot.forEach(function(doc) {
+        //         console.log("deleting " + doc.id);
+        //         fireStore.collection('todoLists').doc(doc.id).delete();
+        //     })
+        // });
+    };
 
     handleReset = () => {
-        const fireStore = getFirestore();
-        let date = new Date();
-        todoJson.todoLists.forEach(todoListJson => {
-            fireStore.collection('todoLists').add({
-                    name: todoListJson.name,
-                    owner: todoListJson.owner,
-                    items: todoListJson.items,
-                    time: date.toString()
-                }).then(() => {
-                    console.log("DATABASE RESET");
-                }).catch((err) => {
-                    console.log(err);
-                });
-        });
-    }
+        // const fireStore = getFirestore();
+        // let date = new Date();
+        // todoJson.todoLists.forEach(todoListJson => {
+        //     fireStore.collection('todoLists').add({
+        //             name: todoListJson.name,
+        //             owner: todoListJson.owner,
+        //             items: todoListJson.items,
+        //             time: date.toString()
+        //         }).then(() => {
+        //             console.log("DATABASE RESET");
+        //         }).catch((err) => {
+        //             console.log(err);
+        //         });
+        // });
+    };
 
     render() {
+        const { auth } = this.props;
+        if (auth.uid) {
+            if(this.state.seconds <= 0) {
+                return (
+                    <Redirect to="/"/>
+                );
+            }
+            else {
+                return (
+                    <div>
+                        <h3>You have no permission, redirecting back to home page in <span style={{color:"red"}}>{this.state.seconds}</span></h3>
+                    </div>
+                )
+            }
+
+        }
         return (
             <div>
                 <button onClick={this.handleClear}>Clear Database</button>
                 <button onClick={this.handleReset}>Reset Database</button>
-            </div>)
+            </div>
+        )
     }
 }
 
@@ -49,6 +87,6 @@ const mapStateToProps = function (state) {
         auth: state.firebase.auth,
         firebase: state.firebase
     };
-}
+};
 
 export default connect(mapStateToProps)(DatabaseTester);
