af502db55c39cbd4035052c237cc0c37d61effd0
ZP4 <jasonhuzheming9@gmail.com>
2019-12-12 17:07:59 -0500

    Updated firebase configs for project

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 var firebaseConfig = {
-    apiKey: "AIzaSyAUHaEIUHWrq8C0Uc2uopc2FGz3_ipi_Jk",
-    authDomain: "cse316finalproject-6da0b.firebaseapp.com",
-    databaseURL: "https://cse316finalproject-6da0b.firebaseio.com",
-    projectId: "cse316finalproject-6da0b",
-    storageBucket: "cse316finalproject-6da0b.appspot.com",
-    messagingSenderId: "1002398966076",
-    appId: "1:1002398966076:web:9f7f9a15e7d548a341be39",
-    measurementId: "G-5Z3MD2LF7L"
+    apiKey: "AIzaSyCQl1Y3gm_nd3D72tGCvsBiS5J7MrXhA1Q",
+    authDomain: "wireframeproject-ef810.firebaseapp.com",
+    databaseURL: "https://wireframeproject-ef810.firebaseio.com",
+    projectId: "wireframeproject-ef810",
+    storageBucket: "wireframeproject-ef810.appspot.com",
+    messagingSenderId: "492612799429",
+    appId: "1:492612799429:web:ea9fa3ae89748b57a859ac",
+    measurementId: "G-VEK9F6RMWK"
 };
 firebase.initializeApp(firebaseConfig);
 
